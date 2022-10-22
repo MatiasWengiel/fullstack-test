d96aa0cf2326406d1fa55327dd92ad3fad556951
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-22 12:24:04 -0500

    Change variable name user to author for consistency with db

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,9 +1,9 @@
 import { Card } from "react-bootstrap";
 export default function Comment(props) {
-  const { user, text, sent } = { ...props };
+  const { author, text, sent } = { ...props };
   return (
     <Card bg="Light" className="mx-auto my-3 w-80">
-      <Card.Header>Message by: {user}</Card.Header>
+      <Card.Header>Message by: {author}</Card.Header>
       <Card.Body style={{ width: "80%", margin: "auto" }}>
         <Card.Text>{text}</Card.Text>
       </Card.Body>
