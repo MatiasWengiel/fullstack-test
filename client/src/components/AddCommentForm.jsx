33a43628e684e1b9ca81e21876975949bc42ba21
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-23 18:25:45 -0500

    Move form functions to custom hook

--- client/src/components/AddCommentForm.jsx
+++ client/src/components/AddCommentForm.jsx
@@ -1,51 +1,10 @@
-import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import axios from "axios";
+import useFormFunctions from "../hooks/useFormFunctions";
 
 export default function AddCommentForm(props) {
-  const { updateCommentsList } = props;
-
-  const [commentData, setCommentData] = useState({
-    author: "",
-    commentText: "",
-  });
-
-  const handleChange = (event) => {
-    event.preventDefault();
-    setCommentData({ ...commentData, [event.target.name]: event.target.value });
-  };
-  const updateDB = () => {
-    return axios.post(
-      `${process.env.REACT_APP_BASE_URL}/api/comments`,
-      {
-        author: commentData.author || "Anonymous User",
-        commentText: commentData.commentText,
-      },
-      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
-    );
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const currentTime = new Date().toString();
-    const form = event.currentTarget;
-    if (form.checkValidity === false) {
-      event.stopPropagation();
-      return;
-    }
-    updateDB()
-      .then(() => {
-        updateCommentsList({
-          //Use time as temporary ID, as it will be unique between comments
-          _id: currentTime,
-          author: commentData.author || "Anonymous User",
-          commentText: commentData.commentText,
-          date: currentTime,
-        });
-        setCommentData({ author: "", commentText: "" });
-      })
-      .catch((error) => console.log(error));
-  };
+  const { handleSubmit, handleChange, commentData } = useFormFunctions(
+    props.updateCommentsList
+  );
 
   return (
     <>
---
+++ client/src/hooks/useFormFunctions.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function useFormFunctions(updateCommentsList) {
+  const [commentData, setCommentData] = useState({
+    author: "",
+    commentText: "",
+  });
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setCommentData({ ...commentData, [event.target.name]: event.target.value });
+  };
+  const updateDB = () => {
+    return axios.post(
+      `${process.env.REACT_APP_BASE_URL}/api/comments`,
+      {
+        author: commentData.author || "Anonymous User",
+        commentText: commentData.commentText,
+      },
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const currentTime = new Date().toString();
+    const form = event.currentTarget;
+    if (form.checkValidity === false) {
+      event.stopPropagation();
+      return;
+    }
+    updateDB()
+      .then(() => {
+        updateCommentsList({
+          //Use time as temporary ID, as it will be unique between comments
+          _id: currentTime,
+          author: commentData.author || "Anonymous User",
+          commentText: commentData.commentText,
+          date: currentTime,
+        });
+        setCommentData({ author: "", commentText: "" });
+      })
+      .catch((error) => console.log(error));
+  };
+  return { handleSubmit, handleChange, commentData };
+}
