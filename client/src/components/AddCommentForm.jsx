edd9f62d96bf69a806dae5799fd7eba061a5f8a2
Matias Wengiel <matiaswengiel@gmail.com>
2022-10-23 10:58:11 -0500

    Remove unnecessary imports

--- client/src/components/AddCommentForm.jsx
+++ client/src/components/AddCommentForm.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import axios from "axios";
 import useFormData from "../hooks/useFormData";
 
 export default function AddCommentForm(props) {
